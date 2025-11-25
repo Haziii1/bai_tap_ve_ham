2bd4a2abdf9e2ddacd92ffa01852a641d3827c85
Haziii1 <nguyenduchaikm1@gmail.com>
2025-11-25 15:46:51 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
-int KiemTraChanLe(int n) {
-	if ( n % 2 == 0 ) return 1;
-	else return 0;
+#include <stdbool.h>
+bool KiemTraChanLe(int n) {
+	return ( n % 2 == 0 );
 }
 int main() {
-	int x;
-	printf("Nhap n: ");  	scanf("%d", &x);
-	if ( KiemTraChanLe(x) == 1) {
-		printf("%d la so chan", x);
-	} else {
-		printf("%d la so le", x);
-	}
+	int n;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	if (KiemTraChanLe(n) ) {
+		printf("%d la so chan", n);
+			} 
+	else {
+		printf("%d la so le", n);
+			}
 return 0;
 }
