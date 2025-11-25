c853f852b51cf7512eb8f7b4bafdc9ad176bd756
Haziii1 <nguyenduchaikm1@gmail.com>
2025-11-25 15:56:25 +0700

    Update bai7.cpp

--- bai7.cpp
+++ bai7.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
-int SoHoanHao(int n) {
+#include <stdbool.h>
+bool SoHoanHao(int n) {
 	int sum = 0;
 	for (int i = 1; i <= n/2 ; i++) {
 		if ( n % i == 0) sum += i;
 	}
 	if (n == sum) 
-	return 1;
+	return true;
 	else
-	return 0;
+	return false;
 }
 int main()
 {
-	int x;
+	int n;
 	printf("Nhap n: ");
-	scanf("%d", &x);
+	scanf("%d", &n);
 	
-	if ( SoHoanHao(x) == 1) {
-		printf("%d la so hoan hao", x);
+	if ( SoHoanHao(n)) {
+		printf("%d la so hoan hao", n);
 	} else {
-		printf("%d khong la so hoan hao", x);
+		printf("%d khong la so hoan hao", n);
 	}
 return 0;
 }
